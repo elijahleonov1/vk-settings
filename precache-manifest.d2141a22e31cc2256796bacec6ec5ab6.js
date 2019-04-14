508e41a69449d52797a6f94fda97e6f8a4e4be56
elijahleonov2 <leonov_ilya@radario.co>
2019-04-14 21:05:03 +0300

    1

---
+++ precache-manifest.d2141a22e31cc2256796bacec6ec5ab6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-settings/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "579c45c53bc7df4dc05e",
+    "url": "/vk-settings/static/js/runtime~main.579c45c5.js"
+  },
+  {
+    "revision": "e0a0883856ae55aefce8",
+    "url": "/vk-settings/static/js/main.e0a08838.chunk.js"
+  },
+  {
+    "revision": "dfce68b63fdbfc06c286",
+    "url": "/vk-settings/static/js/2.dfce68b6.chunk.js"
+  },
+  {
+    "revision": "e0a0883856ae55aefce8",
+    "url": "/vk-settings/static/css/main.97394b36.chunk.css"
+  },
+  {
+    "revision": "dfce68b63fdbfc06c286",
+    "url": "/vk-settings/static/css/2.89625e16.chunk.css"
+  },
+  {
+    "revision": "07076c402911a3fe637971e8ce9fbe35",
+    "url": "/vk-settings/index.html"
+  }
+];
