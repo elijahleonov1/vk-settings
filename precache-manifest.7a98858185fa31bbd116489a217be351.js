401c06963b51d6c714fd3ce58f1a1adc2cbb4e47
elijahleonov2 <leonov_ilya@radario.co>
2019-04-16 19:46:45 +0300

    qw

---
+++ precache-manifest.7a98858185fa31bbd116489a217be351.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "579c45c53bc7df4dc05e",
+    "url": "/vk-settings/static/js/runtime~main.579c45c5.js"
+  },
+  {
+    "revision": "cf7d01c9578d1d8725f5",
+    "url": "/vk-settings/static/js/main.cf7d01c9.chunk.js"
+  },
+  {
+    "revision": "9a5e012644668378eb63",
+    "url": "/vk-settings/static/js/2.9a5e0126.chunk.js"
+  },
+  {
+    "revision": "9a5e012644668378eb63",
+    "url": "/vk-settings/static/css/2.89625e16.chunk.css"
+  },
+  {
+    "revision": "b9e4eabb39c50eaf0c275ad6b5d3f4b5",
+    "url": "/vk-settings/index.html"
+  }
+];
