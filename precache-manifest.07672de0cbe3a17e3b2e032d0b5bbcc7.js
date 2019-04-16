ad519539504f0789b6c7863e1a610724d25fe281
elijahleonov2 <leonov_ilya@radario.co>
2019-04-16 14:29:56 +0300

    22

---
+++ precache-manifest.07672de0cbe3a17e3b2e032d0b5bbcc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "579c45c53bc7df4dc05e",
+    "url": "/vk-settings/static/js/runtime~main.579c45c5.js"
+  },
+  {
+    "revision": "1dcb4dc4f341cfa4b29b",
+    "url": "/vk-settings/static/js/main.1dcb4dc4.chunk.js"
+  },
+  {
+    "revision": "3b9cc3e2c743f3b75296",
+    "url": "/vk-settings/static/js/2.3b9cc3e2.chunk.js"
+  },
+  {
+    "revision": "3b9cc3e2c743f3b75296",
+    "url": "/vk-settings/static/css/2.89625e16.chunk.css"
+  },
+  {
+    "revision": "c1eb3494de798b3ed03619cff7bb0b3d",
+    "url": "/vk-settings/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vk-settings/precache-manifest.5171eae781b8319d7be18b1e1dd23aed.js"
+  "/vk-settings/precache-manifest.07672de0cbe3a17e3b2e032d0b5bbcc7.js"
 );
 
 workbox.clientsClaim();
