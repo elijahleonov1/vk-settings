734b1f068de8e16ac50db104b514c5c2d067834a
elijahleonov1 <elijah.leonov@gmail.com>
2019-04-20 21:54:02 +0300

    s

---
+++ precache-manifest.821a812c303bbdf98fdfdf68a6a5ae06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-settings/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "579c45c53bc7df4dc05e",
+    "url": "/vk-settings/static/js/runtime~main.579c45c5.js"
+  },
+  {
+    "revision": "be1ec84a2d82210b4897",
+    "url": "/vk-settings/static/js/main.be1ec84a.chunk.js"
+  },
+  {
+    "revision": "ae755127b4a2b222ee7b",
+    "url": "/vk-settings/static/js/2.ae755127.chunk.js"
+  },
+  {
+    "revision": "be1ec84a2d82210b4897",
+    "url": "/vk-settings/static/css/main.0dff559a.chunk.css"
+  },
+  {
+    "revision": "ae755127b4a2b222ee7b",
+    "url": "/vk-settings/static/css/2.2bf2b98f.chunk.css"
+  },
+  {
+    "revision": "af588155ca8f495786098fcdf5e6c765",
+    "url": "/vk-settings/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vk-settings/precache-manifest.1104cf18e40681fb57ee72556e38646e.js"
+  "/vk-settings/precache-manifest.821a812c303bbdf98fdfdf68a6a5ae06.js"
 );
 
 workbox.clientsClaim();
