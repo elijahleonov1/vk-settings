ac3821dcbdc4a4a0dbe18b04918134f04cabc655
elijahleonov1 <elijah.leonov@gmail.com>
2019-04-20 16:48:30 +0300

    s

---
+++ precache-manifest.2eeb14bddbfb1ba5deb79ea1b6da3edd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "f9aadbd25d824eeddaaa",
+    "url": "/static/js/main.f9aadbd2.chunk.js"
+  },
+  {
+    "revision": "2142f1044b4e56f30952",
+    "url": "/static/js/2.2142f104.chunk.js"
+  },
+  {
+    "revision": "f9aadbd25d824eeddaaa",
+    "url": "/static/css/main.0dff559a.chunk.css"
+  },
+  {
+    "revision": "2142f1044b4e56f30952",
+    "url": "/static/css/2.2bf2b98f.chunk.css"
+  },
+  {
+    "revision": "6ac3ff30b09ae8e586b0deda540b936f",
+    "url": "/index.html"
+  }
+];
