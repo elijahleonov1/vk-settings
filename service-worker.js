cf92bdfa6aa893612fe811bcd5b4847bf18b39e0
elijahleonov1 <elijah.leonov@gmail.com>
2019-04-20 22:06:19 +0300

    s

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vk-settings/precache-manifest.67de496638366f19096fb6eba4b5bff2.js"
+  "/vk-settings/precache-manifest.1104cf18e40681fb57ee72556e38646e.js"
 );
 
 workbox.clientsClaim();
