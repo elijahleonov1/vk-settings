a84a460a5e8204bd6306a937d8fc839f1a0654c5
elijahleonov1 <elijah.leonov@gmail.com>
2019-04-22 00:04:33 +0300

    asdasd

---
+++ precache-manifest.603f04a9bf98a5df3e800e489863ae5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "579c45c53bc7df4dc05e",
+    "url": "/vk-settings/static/js/runtime~main.579c45c5.js"
+  },
+  {
+    "revision": "c22539488f23d3199254",
+    "url": "/vk-settings/static/js/main.c2253948.chunk.js"
+  },
+  {
+    "revision": "a74b2d570ceec844638a",
+    "url": "/vk-settings/static/js/2.a74b2d57.chunk.js"
+  },
+  {
+    "revision": "a74b2d570ceec844638a",
+    "url": "/vk-settings/static/css/2.2bf2b98f.chunk.css"
+  },
+  {
+    "revision": "a67b4cf4a57ab4d5643f554289979afd",
+    "url": "/vk-settings/index.html"
+  }
+];
