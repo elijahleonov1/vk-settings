627a8daae65e3143141c71c578c2f5de0851c406
elijah <ex@gmail.com>
2019-07-28 18:38:44 +0300

    asd
    asd

---
+++ precache-manifest.57a49aba1545d42ce5197bba523d4edd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-settings/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "579c45c53bc7df4dc05e",
+    "url": "/vk-settings/static/js/runtime~main.579c45c5.js"
+  },
+  {
+    "revision": "19dae45fb87e4d4fab06",
+    "url": "/vk-settings/static/js/main.19dae45f.chunk.js"
+  },
+  {
+    "revision": "978a663cc2f63784f7dc",
+    "url": "/vk-settings/static/js/2.978a663c.chunk.js"
+  },
+  {
+    "revision": "19dae45fb87e4d4fab06",
+    "url": "/vk-settings/static/css/main.4f18a2bd.chunk.css"
+  },
+  {
+    "revision": "978a663cc2f63784f7dc",
+    "url": "/vk-settings/static/css/2.91a88d4c.chunk.css"
+  },
+  {
+    "revision": "fb7a28f37561230b3e5c912ec5c49168",
+    "url": "/vk-settings/index.html"
+  }
+];
