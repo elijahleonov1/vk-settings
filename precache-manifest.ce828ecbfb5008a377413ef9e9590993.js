0720424e83249d65a8ae758b2bfec15c27ca8a5b
elijah <ex@gmail.com>
2019-07-28 14:39:39 +0300

    asd
    asdasd

---
+++ precache-manifest.ce828ecbfb5008a377413ef9e9590993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f0c20d41f92268182e83",
+    "url": "/static/js/main.3dd46de1.chunk.js"
+  },
+  {
+    "revision": "3db65aac0db5224a40c1",
+    "url": "/static/js/2.1a767723.chunk.js"
+  },
+  {
+    "revision": "3db65aac0db5224a40c1",
+    "url": "/static/css/2.a8b97b5a.chunk.css"
+  },
+  {
+    "revision": "4065dbb5dc9b3842f91da2b7adc25799",
+    "url": "/index.html"
+  }
+];
