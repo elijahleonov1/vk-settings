0b702031829205b48c4c0a9dae458b8fb94b69d8
elijah <ex@gmail.com>
2019-08-01 23:55:11 +0300

    asd
    asd

---
+++ precache-manifest.737adcb6aa632794491fbb61239d23d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "579c45c53bc7df4dc05e",
+    "url": "/vk-settings/static/js/runtime~main.87dce8bd.js"
+  },
+  {
+    "revision": "6136d4c38ca738123157",
+    "url": "/vk-settings/static/js/main.cc39346a.chunk.js"
+  },
+  {
+    "revision": "4f911c14f29657d5e183",
+    "url": "/vk-settings/static/js/2.0008f412.chunk.js"
+  },
+  {
+    "revision": "4f911c14f29657d5e183",
+    "url": "/vk-settings/static/css/2.8ef47eb5.chunk.css"
+  },
+  {
+    "revision": "5ce89337a1ec49012f5cf5ecadcb9ce5",
+    "url": "/vk-settings/index.html"
+  }
+];
