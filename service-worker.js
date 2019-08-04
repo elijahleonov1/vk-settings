6a326f100d588fe46fea5d39870bab1684ca56d6
elijah <ex@gmail.com>
2019-08-04 23:35:34 +0300

    asdasda
    asdasd

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vk-settings/precache-manifest.744547419019dec03b6438d5cb61c867.js"
+  "/vk-settings/precache-manifest.abb65f3eebbac5aa3bc8a2741022da35.js"
 );
 
 workbox.clientsClaim();
