d1d8d72a074bef0f250b719a6d3bc1f33e38dd73
elijah <ex@gmail.com>
2019-08-18 18:58:13 +0300

    sasdasd

--- precache-manifest.abd108c7c650733ae19ae23aaa789c7e.js
+++ precache-manifest.abd108c7c650733ae19ae23aaa789c7e.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/vk-settings/static/js/runtime~main.87dce8bd.js"
   },
   {
-    "revision": "ccf8689ed0e637d368fc",
-    "url": "/vk-settings/static/js/main.b1944960.chunk.js"
+    "revision": "9809dd28b879e70f342f",
+    "url": "/vk-settings/static/js/main.94b0f8e3.chunk.js"
   },
   {
     "revision": "2f9fc3837ed2d47f70a5",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/vk-settings/static/css/2.8ef47eb5.chunk.css"
   },
   {
-    "revision": "94d772463fbb88d7058d80be84e27ee1",
+    "revision": "886bdb70e94c48820b0ffa8c5e66e11f",
     "url": "/vk-settings/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vk-settings/precache-manifest.199ce012185bb8e1bf1e5b0f61983db1.js"
+  "/vk-settings/precache-manifest.abd108c7c650733ae19ae23aaa789c7e.js"
 );
 
 workbox.clientsClaim();
